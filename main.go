3fd452be252b2b837fa8de06fc71c0c5694deb2d
Adil Farooque <Adilfarq11@gmail.com>
2024-01-23 10:11:57 +0530

    Connected to PostgresDB

--- main.go
+++ main.go
@@ -1,6 +1,13 @@
 package main
 
-//stdudent records
+import (
+	"fmt"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+// stdudent records
 var id int
 var name, domain string
 
@@ -13,4 +20,13 @@ type Student struct {
 func main() {
 
 }
+// connecting to db with gorm pkg
+func connectPostgresDB() *gorm.DB {
+	connectTo := "host=localhost user=postgres password=******** dbname=databasename port=5432 sslmode=disable"
 
+	db, err := gorm.Open(postgres.Open(connectTo), &gorm.Config{})
+	if err != nil {
+		fmt.Println(err)
+	}
+	return db
+}
