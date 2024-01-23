f889b9a0d1613345d992f009ebb3d8e68cba9c7a
Adil Farooque <Adilfarq11@gmail.com>
2024-01-23 10:03:18 +0530

    First commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Adilfarooque/CRUD_gorm
+
+go 1.21.5
---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+//stdudent records
+var id int
+var name, domain string
+
+type Student struct {
+	Id     int
+	Name   string
+	Domain string
+}
+
+func main() {
+
+}
+
